462ab72d0267645075302f7ab9b192cfc21fd5dd
sandesh shrestha <sandesh.shrestha@uconn.edu>
2016-10-08 12:17:07 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sandesh/foodhutt/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.sandesh.foodhutt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Switch switch1 = (Switch) findViewById(R.id.switch1);
+
+        final Button btRegister = (Button) findViewById(R.id.btRegister);
+    }
+}
